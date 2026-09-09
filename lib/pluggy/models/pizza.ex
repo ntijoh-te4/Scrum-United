4effc406c6a4c82d86e6113eb22d8cbc0a6fdce3
Viktor Österberg <viktor.osterberg@viktorosterberg-T2QCX9CH4X.local>
2026-09-09 15:49:26 +0200

    merge aswell as copy of fruit pizzaified

--- lib/pluggy/models/pizza.ex
+++ lib/pluggy/models/pizza.ex
@@ -1,4 +1,56 @@
 defmodule Pluggy.Pizza do
+  defstruct(id: nil, name: "")
+
+  alias Pluggy.Pizza
+
+  def all do
+    Postgrex.query!(DB, "SELECT * FROM pizzas", []).rows
+    |> to_struct_list
+  end
+
+  def get(id) do
+    Postgrex.query!(DB, "SELECT * FROM pizzas WHERE id = $1 LIMIT 1", [String.to_integer(id)]).rows
+    |> to_struct
+  end
+
+  def update(id, params) do
+    name = params["name"]
+    id = String.to_integer(id)
+
+    Postgrex.query!(
+      DB,
+      "UPDATE pizzas SET name = $1 WHERE id = $2",
+      [name, id]
+    )
+  end
+
+  def create(params) do
+    name = params["name"]
+
+    Postgrex.query!(DB, "INSERT INTO pizzas (name) VALUES ($1)", [name])
+  end
+
+  def delete(id) do
+    Postgrex.query!(DB, "DELETE FROM pizzas WHERE id = $1", [String.to_integer(id)])
+  end
+
+  def to_struct([[id, name]]) do
+    %Pizza{id: id, name: name}
+  end
+
+  def to_struct_list(rows) do
+    for [id, name] <- rows, do: %Pizza{id: id, name: name}
+  end
+
+
+
+
+
+
+
+
+
+
 
   def get_name(id) do
     Postgrex.query!(DB, "SELECT name FROM pizzas WHERE id = $1 LIMIT 1", [id]).rows

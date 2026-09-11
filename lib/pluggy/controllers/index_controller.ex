75e39044d4da40511e434af665ffe6a35f42b18a
ntijoh-pontus-martinsson <pontus.martinsson@elev.ga.ntig.se>
2026-09-11 10:52:11 +0200

    Remove unused route + controller

--- lib/pluggy/controllers/index_controller.ex
+++
@@ -1,5 +0,0 @@
- defmodule Pluggy.IndexController do
-     import Plug.Conn, only: [send_resp: 3]
-     import Pluggy.Template, only: [render: 1]
-   def index(conn), do: send_resp(conn, 200, render("index"))
- end
--- lib/pluggy/router.ex
+++ lib/pluggy/router.ex
@@ -6,7 +6,6 @@ defmodule Pluggy.Router do
   alias Pluggy.UserController
   alias Pluggy.PizzaController
   alias Pluggy.CartController
-  #alias Pluggy.IndexController
   alias Pluggy.AdminController
 
   plug(Plug.Static, at: "/", from: :pluggy)
@@ -28,7 +27,6 @@ defmodule Pluggy.Router do
 
   # Sessions are demonstrated by the login form, but no route checks for a logged-in user:
   # anyone can create, edit and destroy fruits.
-  #get("/", do: IndexController.index(conn))
   get("/fruits", do: FruitController.index(conn))
   get("/fruits/new", do: FruitController.new(conn))
   get("/fruits/:id", do: FruitController.show(conn, id))
